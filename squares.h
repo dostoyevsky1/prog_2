c9089a345326f6b3e551d0d3be14f045cba0a99c
Michael Drozdov <mdrozdov94@gmail.com>
2019-10-20 21:20:53 -0400

    instantiated header file and ssquares_helper.cpp with support function definitions

---
+++ squares.h
@@ -0,0 +1,12 @@
+// Absolute value function
+double abs(double val);
+
+// Power Function
+double Power(double b, int e);
+
+// Square Root Function
+double Sqrt(double v, int initial_guess = 1);
+
+// More Data Function
+bool MoreData();
+
